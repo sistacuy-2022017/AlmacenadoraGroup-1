955610e97d3572d9d30eb3c9d83e8408a346349b
Alejandro <amax-2019189@kinal.edu.gt>
2024-04-25 20:51:48 -0600

    Se agrego la validacion de las prioridades

--- Backend/src/warehousing/warehousing.routes.js
+++ Backend/src/warehousing/warehousing.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import { validateProgressTask } from "../middlewares/validation-Progress.js";
+import { typePriority } from '../middlewares/validation-Priority.js'
 import { createTask, updateTask } from "./warehousing.controller.js";
 const routesWarehousing = Router();
 
-routesWarehousing.post("/createTask", [], validateProgressTask, createTask);
+routesWarehousing.post("/createTask", [], validateProgressTask, typePriority, createTask);
 
 routesWarehousing.put("/updateTask/:id", [], updateTask);
 
