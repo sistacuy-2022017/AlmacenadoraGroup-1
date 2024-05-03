8ca799cecfab1dd859b9e60701a01287a3c0f8aa
dalvarez-2022277 <dalvarez-2022277@kinal.edu.gt>
2024-05-02 21:32:40 -0600

    se agrega backend finalizado

--- src/warehousing/warehousing.controller.js
+++ src/warehousing/warehousing.controller.js
@@ -39,14 +39,9 @@ export const createTask = async (req, res) => {
 
 export const updateTask = async (req, res) => {
   const { id } = req.params;
-  const {
-    _id,
-    statusTask,
-    ...resto
-  } = req.body;
+  const { _id, statusTask, ...resto } = req.body;
 
   try {
-     
     const task = await WareHousing.findById(id);
 
     if (!task) {
@@ -55,7 +50,6 @@ export const updateTask = async (req, res) => {
 
     await WareHousing.findByIdAndUpdate(id, resto);
     const task11 = await WareHousing.findOne({ _id: id });
-
 
     res.status(200).json({ msg: "Tarea actualizada exitosamente", task11 });
   } catch (error) {
@@ -77,6 +71,17 @@ export const deleteTask = async (req, res) => {
     res.status(200).json({ msg: "Tarea eliminada exitosamente" });
   } catch (error) {
     console.error("Error al eliminar la tarea: ", error);
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await WareHousing.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Error al obtener las tareas: ", error);
     res.status(400).json({ error: error.message });
   }
 };
--- src/warehousing/warehousing.routes.js
+++ src/warehousing/warehousing.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { createTask, updateTask, deleteTask } from "./warehousing.controller.js";
+import { createTask, updateTask, deleteTask, getAllTasks} from "./warehousing.controller.js";
 const routesWarehousing = Router();
 
 routesWarehousing.post("/createTask", [], createTask);
@@ -10,6 +10,6 @@ routesWarehousing.put("/updateTask/:id", [], updateTask);
 
 routesWarehousing.delete("/deleteTask/:id", [], deleteTask);
 
-
+routesWarehousing.get("/getTask", [], getAllTasks);
 
 export default routesWarehousing;
